ec99682c86ab84b30a891fbeda8894ac31d84bd0
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2021-07-13 16:53:19 -0500

    Update

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const btnCheck = document.querySelector('#btnCheck')
-const txtName = document.querySelector("#txtName") 
-const txtUsername = document.querySelector("#txtUsername") 
+const txtName = document.querySelector("#txtName")
+const txtUsername = document.querySelector("#txtUsername")
 const txtDescription = document.querySelector("#txtDescription")
 const txtDate = document.querySelector("#txtDate")
 const imgThumb = document.querySelector("#imgThumb")
-  
+
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('/logface/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/logface/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/logface/models')
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
+
 
 async function start() {
   const speech = new SpeechSynthesisUtterance();
@@ -19,22 +20,22 @@ async function start() {
   const labeledFaceDescriptors = await loadLabeledImages()
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
   let canvas
+  alert("Listo")
   btnCheck.addEventListener('click', async () => {
     canvas = faceapi.createCanvasFromMedia(document.querySelector("#video"))
     const detections = await faceapi.detectAllFaces(canvas).withFaceLandmarks().withFaceDescriptors()
     const displaySize = { width: canvas.width, height: canvas.height }
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    
-    if(results[0].label != "unknown"){
+    if (results[0].label != "unknown") {
+      window.speechSynthesis.speak(speech)
       speech.text = "Acceso permitido, bienvenido, " + results[0].label
-      imgThumb.src = `labeled_images/${ results[0].label }/1.jpg`
+      imgThumb.src = `labeled_images/${results[0].label}/1.jpg`
       txtName.innerHTML = results[0].label
       txtUsername.innerHTML = "@" + results[0].label
       txtDescription.innerHTML = "Empleado de sistemas"
       txtDate.innerHTML = new Date()
-      window.speechSynthesis.speak(speech)
-    }else{
+    } else {
       speech.text = "Acceso denegado"
       imgThumb.src = "https://bulma.io/images/placeholders/96x96.png"
       txtName.innerHTML = "Desconocido"
@@ -43,17 +44,11 @@ async function start() {
       txtDate.innerHTML = ""
       window.speechSynthesis.speak(speech)
     }
-    
-   
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
   })
+
 }
 
-function loadLabeledImages() {
+async function loadLabeledImages() {
   const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Fazur', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
   return Promise.all(
     labels.map(async label => {
